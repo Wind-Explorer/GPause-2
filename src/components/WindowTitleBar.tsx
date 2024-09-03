873abbefaf006b29aaee403103e4c51fd63b1be6
Adam C. <windexplorer@outlook.com>
2024-09-04 01:13:33 +0800

    App metadata

--- src/components/WindowTitleBar.tsx
+++ src/components/WindowTitleBar.tsx
@@ -13,16 +13,21 @@ import {
 } from "@nextui-org/react";
 import { InformationCircleIcon } from "../icons";
 import { open } from "@tauri-apps/api/shell";
+import { getVersion } from "@tauri-apps/api/app";
 
 export default function WindowTitleBar() {
   const [osType, setOsType] = useState<string>("");
   const [isMaximized, setIsMaximized] = useState<boolean>(false);
+  const [appVersion, setAppVersion] = useState<string>("");
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   useEffect(() => {
     os.type().then((type) => {
       setOsType(type);
+    });
+    getVersion().then((version) => {
+      setAppVersion(version);
     });
   }, []);
 
@@ -132,7 +137,10 @@ export default function WindowTitleBar() {
                       alt="GPause Icon"
                     />
                     <div className="flex flex-col gap-4 text-center *:mx-auto">
-                      <p className="text-2xl font-bold">GPause 2</p>
+                      <div className="flex flex-col">
+                        <p className="text-2xl font-bold">GPause 2</p>
+                        <p className="opacity-50">Build {appVersion}</p>
+                      </div>
 
                       <div className="flex flex-col gap-2">
                         <p>Put your opened apps and games to sleep</p>
@@ -140,7 +148,7 @@ export default function WindowTitleBar() {
                       </div>
                       <Link
                         onPress={() => {
-                          open("https://github.com/wind-explorer/gpause-2");
+                          open("https://github.com/wind-explorer/GPause-2");
                         }}
                       >
                         View on GitHub
